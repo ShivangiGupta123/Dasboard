222932036d47eb46809d17a824d7433a594394c5
Kamlesh Gupta <kamleshgupta594@gmail.com>
2024-08-18 17:02:31 +0530

    added changes in sidebar

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import './Sidebar.css'
+import { FlashCircle, Home2 } from 'iconsax-react'
 const Sidebar = () => {
   return (
     <div className='sidebar-container'>
-      Sidebar
+      <div className='border px-2 py-2 mt-2 d-flex align-items-center justify-content-left'>
+        <FlashCircle size="20" color="#FF8A65" />
+        <span className='text-white' >Kamlesh</span>
+      </div>
+      <div className='mt-4'>
+      </div>
+
+      <div className='border px-2 py-2 mt-2 d-flex align-items-center justify-content-left rounded-4'>
+        <Home2 size="20" color="#FF8A65" />
+        <span className='text-white mx-2' >Kamlesh</span>
+      </div>
+
+      <div className='border px-2 py-2 mt-2 d-flex align-items-center justify-content-left rounded-4'>
+        <Home2 size="20" color="#FF8A65" />
+        <span className='text-white mx-2' >Kamlesh</span>
+      </div>
+      <div className='border px-2 py-2 mt-2 d-flex align-items-center justify-content-left rounded-4'>
+        <Home2 size="20" color="#FF8A65" />
+        <span className='text-white mx-2' >Kamlesh</span>
+      </div>
+
     </div>
   )
-}
+};
 
 export default Sidebar
